61bfd79ffe48176832a9e6e751dc9dc86a5bca4d
Lgotin Bogdan <lgotindev@gmail.com>
2019-08-20 20:10:09 +0600

    fix find

--- main.php
+++ main.php
@@ -2,21 +2,28 @@
 
 namespace Main;
 
-function searchFile(string $path = "datafiles/", string $findFile = ".ixt")
+function findFile(string $dirPath = "datafiles/", string $findFile = ".ixt")
 {
+    $result = [];
     $fileList = [];
-    if ($currentDir = opendir($path)) {
+    if ($currentDir = opendir($dirPath)) {
         while ($file = readdir($currentDir)) {
             if (strpos($file, $findFile) === strlen($file) - strlen($findFile)) {
                 $fileList[] = $file;
             }
         }
-        closedir($currentDir);
-        asort($fileList, SORT_STRING);
     }
-    return $fileList;
+    closedir($currentDir);
+    asort($fileList, SORT_STRING);
+    foreach ($fileList as $word) {
+        $fullName = explode('.', $word);
+        if ((preg_match("/[a-zA-Z0-9]/", $fullName[0]))) {
+            $result[] = $fullName[0] . "{$findFile}";
+        }
+    }
+    return $result;
 }
 
-print_r(searchFile());
+print_r(findFile());
 
 
