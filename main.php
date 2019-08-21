1235d2ba90ee191a3f8757306843433367659d67
Bogdan <38589238+Lgotindev@users.noreply.github.com>
2019-08-21 20:57:57 +0600

    Update main.php

--- main.php
+++ main.php
@@ -9,7 +9,7 @@ function findFileNames(string $dirPath, array $validExtensions = ['ixt'])
     $correctFilesNames = array_filter($filesNames, function($file) use ($validExtensions) {
         $extension = pathinfo($file, PATHINFO_EXTENSION);
         $fileName = pathinfo($file, PATHINFO_FILENAME);
-            return in_array($extension, $validExtensions) && preg_match("/[a-zA-Z0-9]/", $fileName);
+        return in_array($extension, $validExtensions) && preg_match("/[a-zA-Z0-9]/", $fileName);
     });
 
     return array_values($correctFilesNames);
